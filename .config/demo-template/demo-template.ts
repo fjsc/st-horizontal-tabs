8ed6023b5c1e023d058810037dbb766292531058
Abi <elabi3@gmail.com>
2020-06-07 16:22:44 +0200

    feat(demo): adding design tab and splitting source & demo code

--- .config/demo-template/demo-template.ts
+++ .config/demo-template/demo-template.ts
@@ -9,14 +9,16 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-import { LitElement, html, customElement, TemplateResult, property, css, internalProperty, unsafeCSS } from 'lit-element';
+import {css, customElement, html, internalProperty, LitElement, property, TemplateResult, unsafeCSS} from 'lit-element';
 import '../../src/components/st-horizontal-tabs';
 import styles from './demo-template.css';
 
 enum TemplateSections {
-    demo = 'Demo',
-    api = 'Api',
-    sourceCode = 'Source Code'
+  design = 'Design',
+  demo = 'Demo',
+  demoSourceCode = 'Demo Code',
+  api = 'Component Api',
+  sourceCode = 'Component Code',
 }
 
 @customElement('demo-template')
@@ -24,6 +26,8 @@ export class DemoTemplate extends LitElement {
 
   @property() componentName: string;
   @property() componentDescription: string;
+  @property() designBehavior: string;
+  @property() demoSourceCode: string;
   @property() sourceCode: string;
 
   static styles = css`${unsafeCSS(styles)}`;
@@ -38,17 +42,21 @@ export class DemoTemplate extends LitElement {
         <hr class="demo-template__hr">
         <div class="demo-template__description">${this.componentDescription}</div>
         <div class="demo-template__separator"></div>
-        
+
         ${this.getTabs()}
-        
+
         <div class="demo-template__content">
-            ${this.currentSection === TemplateSections.sourceCode ? 
-                html`<textarea>${this.sourceCode}</textarea>` : ''
-            }
+            ${this.currentSection === TemplateSections.design ?
+      html`<section><h2>Design & Behavior</h2><p>${this.designBehavior}</p></section>
+           <section><h2>Usage and Best Practices</h2><slot name="usage"></slot></section>` : ''}
+            ${this.currentSection === TemplateSections.demoSourceCode ?
+      html`<textarea>${this.demoSourceCode}</textarea>` : ''}
+            ${this.currentSection === TemplateSections.sourceCode ?
+      html`<textarea>${this.sourceCode}</textarea>` : ''}
             <!--<wc-monaco-editor language="markdown"></wc-monaco-editor>-->
             ${this.currentSection === TemplateSections.demo ? html`
                 <div class="content">
-                    <slot></slot>
+                    <slot name="content"></slot>
                 </div>
             ` : ''}
         </div>
@@ -60,18 +68,18 @@ export class DemoTemplate extends LitElement {
   public getTabs() {
     return html`
       <div class="tabs">
-        ${Object.keys(TemplateSections).map((key: string) => 
-        html `
-            <div @click="${() => this.setTab(TemplateSections[key])}" 
-                class="tab ${this.currentSection === TemplateSections[key] ? 'tab--active': ''}">
+        ${Object.keys(TemplateSections).map((key: string) =>
+      html`
+            <div @click="${() => this.setTab(TemplateSections[key])}"
+                class="tab ${this.currentSection === TemplateSections[key] ? 'tab--active' : ''}">
                 ${TemplateSections[key]}
             </div>
         `)}
       </div>
-    `
+    `;
   }
 
   public setTab(tab: TemplateSections) {
-      this.currentSection = tab;
+    this.currentSection = tab;
   }
 }
--- demo/demo-snippet.ts
+++ demo/demo-snippet.ts
@@ -9,14 +9,16 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-import { LitElement, html, customElement, TemplateResult, internalProperty } from 'lit-element';
-import '../src/components/st-horizontal-tabs'
-import { StHorizontalTab } from '../src/models/st-horizontal-tabs.model';
+import {customElement, html, internalProperty, LitElement, TemplateResult} from 'lit-element';
+import '../src/components/st-horizontal-tabs';
+import {StHorizontalTab} from '../src/models/st-horizontal-tabs.model';
+import '../.config/demo-template/demo-template';
 
 
 /** NOT CHANGE!! */
-const demoCode = require('!!raw-loader!./demo-snippet')
-import '../.config/demo-template/demo-template';
+const demoCode = require('!!raw-loader!./demo-snippet');
+const sourceCode = require('!!raw-loader!../src/components/st-horizontal-tabs');
+
 /** DEMO REQUIRED IMPORTS */
 
 
@@ -26,10 +28,10 @@ export class DemoSnippet extends LitElement {
   @internalProperty()
   protected options: StHorizontalTab[] = [{
     id: 'tab1',
-    text: 'Tab number 1'
+    text: 'Tab number 1',
   }, {
     id: 'tab2',
-    text: 'Tab number 2'
+    text: 'Tab number 2',
   }];
 
   @internalProperty()
@@ -38,19 +40,40 @@ export class DemoSnippet extends LitElement {
   @internalProperty()
   public description: string = 'Horizontal tabs component enables the user to interact with the view. Use when the application requires split view into multiple sections.';
 
+  @internalProperty()
+  public design: string = 'Tabs appear in a single, non-scrollable row, above their content. The width of each tab is dependent on its label. To ensure that all tabs appear in the container, avoid using more than seven tabs and limit labels to one or two words.';
+
   render(): TemplateResult {
     return html`
-      <demo-template 
+      <demo-template
         componentName="St Horizontal Tabs"
         componentDescription="${this.description}"
-        sourceCode="${demoCode.default}">
+        designBehavior="${this.design}"
+        sourceCode="${sourceCode.default}"
+        demoSourceCode="${demoCode.default}">
 
-
-        <st-horizontal-tabs 
+       <section slot="content">
+          <st-horizontal-tabs
           .options="${this.options}"
           .activeOption="${this.activeOption}"
           @changed-option="${this.changedOption}"></st-horizontal-tabs>
+       </section>
 
+       <section slot="usage">
+        <ul class="list">
+            <li> Use tabs for alternate views within the sidenav, the aside or main content area.</li>
+            <li> Don’t use tabs to break user interactions into a series of steps. Serial workflows are best presented in a wizard. </li>
+            <li> Avoid using tabs in cards and modals.</li>
+            <li> Ensure that the content in each view is independent of the content in other views.</li>
+            <li> Don’t force users to navigate back and forth to compare data–keep such content in the same view.</li>
+            <li> Avoid cross-linking between tabs.</li>
+            <li> If the content within a view is broad, divide it into subsections.</li>
+            <li> Users should not have to move between tabs in order to complete a task</li>
+            <li> Favor nouns over verbs, for example, Settings, Permissions, and Performance.</li>
+            <li> Use title-style caps.</li>
+            <li> Avoid using icons in labels.</li>
+         </ul>
+       </section>
 
       </demo-template>
     `;
